603f0484a536c335ffb303366e55b14230397684
Jack Feng <huadong.feng@outlook.com>
2019-10-13 18:30:24 -0500

    N/A

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from core.data_dependency_graph import DDGs
 
 
 def main():
-    contract_dir = './Purchase.sol'
+    contract_dir = './HoloToken.sol'
     data_dependency_graphs = DDGs(contract_dir)
-    contract = data_dependency_graphs.get_contract_by_name('Purchase')
+    contract = data_dependency_graphs.get_contract_by_name('HoloToken')
     print(contract)
 
 
