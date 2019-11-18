3e5c251611a66d308581f7c830af57004ac948ee
Jack Feng <huadong.feng@outlook.com>
2019-11-17 20:04:12 -0600

    Added comments.

--- core/declarations/unary_operation.py
+++ core/declarations/unary_operation.py
@@ -2,6 +2,12 @@ from .operation import Operation
 
 
 class UnaryOperation(Operation):
+    """
+    This class is still yet to be utilized.
+    Currently, we are using slither UnaryOperation directly from slither.
+
+    Finished.
+    """
     def __init__(self):
         super().__init__()
         self.var = None
