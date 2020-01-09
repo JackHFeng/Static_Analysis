22cf8ec26a19ebab2ddd7d124da240e6e3a921b7
Jack Feng <huadong.feng@outlook.com>
2020-01-09 13:57:13 -0600

    Addes doubly linked list from ctfuzz to static analysis

---
+++ core/declarations/dlinked_node.py
@@ -0,0 +1,9 @@
+class DLinkedNode:
+    def __init__(self):
+        self.pc = None
+        self.opcode = None
+        self.size = None
+        self.value = None
+
+        self.pre = None
+        self.next = None
