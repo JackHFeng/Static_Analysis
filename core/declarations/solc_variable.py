31d3d56bbc43bdc82c8bd694e14a76a3f5b7d896
Jack Feng <huadong.feng@outlook.com>
2020-01-12 17:05:02 -0600

    Remove old file

--- core/declarations/solc_variable.py
+++
@@ -1,9 +0,0 @@
-from .local_variable import LocalVariable
-
-class SolcVariable(LocalVariable):
-    def __init__(self):
-        pass
-
-class SolcVariableComposed(LocalVariable):
-    def __init__(self):
-        pass
