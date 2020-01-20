3bc0236c03e1625b5d7dee108f5a05fd2471318a
Jack Feng <huadong.feng@outlook.com>
2020-01-19 18:14:50 -0600

    Changed Solidity Variable Composed as exactly same as Parameter for the moment.

--- core/declarations/solidity_variable_composed.py
+++ core/declarations/solidity_variable_composed.py
@@ -1,15 +1,6 @@
 from slither.core.variables.local_variable import LocalVariable as Slither_Local_Variable
-from .local_variable import LocalVariable
+from .parameter import Parameter
 
 
-class SolidityVariableComposed(LocalVariable):
-    def __init__(self, variable: Slither_Local_Variable):
-        """
-        Still missing
-        original count, count of rep values
-        """
-        super().__init__(variable)
-
-        # list of representative values for the parameter
-        self._rep_values = []
-
+class SolidityVariableComposed(Parameter):
+    pass
