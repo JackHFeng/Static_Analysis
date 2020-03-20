32c45ff2c24751f4b0cb7f43d71aa13c49980749
Jack Feng <huadong.feng@outlook.com>
2020-03-19 22:09:55 -0500

    opcode, included new attributes <source_map, source_code, block, original_function>, new function for set_original_function. For source mapping, function level opcode coverage purpose.

--- core/declarations/opcode.py
+++ core/declarations/opcode.py
@@ -3,10 +3,22 @@ class Opcode:
         self.pc = None
         self.opcode = None
         self.size = None
+        # if opcode is pushx
         self.value = None
+        # original function object
+        self.original_function = None
+        # (offset, length)
+        self.source_map = None
+        self.source_code = None
 
         self.pre = None
         self.next = None
+        self.block = None
+
+    def set_original_function(self, original_function):
+        self.original_function = original_function
+        original_function.add_opcode(self)
+        self.source_code = original_function.parent_contract.source_code_bytes[self.source_map[0]: self.source_map[0] + self.source_map[1]].decode('utf-8')
 
     def __str__(self):
         return f'{self.pc} {self.opcode} {self.value if self.value else ""}'
